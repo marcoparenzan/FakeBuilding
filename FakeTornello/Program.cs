5b6c3d81e6a8fd2b5c201f14eaff5bbe8800660b
Marco Parenzan <marco.parenzan@live.it>
2018-05-31 13:04:37 +0200

    Add project files.

---
+++ FakeTornello/Program.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Azure.Devices.Client;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Configuration;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakeTornello
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            var configuration = builder.Build();
+
+            var deviceId = configuration["deviceId"];
+            var authenticationMethod =
+                new DeviceAuthenticationWithRegistrySymmetricKey(
+                    deviceId,
+                    configuration["deviceKey"]
+                )
+            ;
+
+            var transportType = TransportType.Mqtt;
+            if (!string.IsNullOrWhiteSpace(configuration["transportType"]))
+            {
+                transportType = (TransportType)
+                    Enum.Parse(typeof(TransportType),
+                    configuration["transportType"], true);
+
+            }
+
+            var client = DeviceClient.Create(
+                configuration["hostName"],
+                authenticationMethod,
+                transportType
+            );
+
+            Console.WriteLine($"Welcome to THE COMPANY");
+            Console.WriteLine($"You are in front of the door {configuration["deviceId"]}");
+            //Console.ReadLine();
+
+            while (true)
+            {
+                var message = await client.ReceiveAsync();
+                if (message == null) continue;
+
+                var bytes = message.GetBytes();
+                if (bytes == null) continue;
+
+                var text = Encoding.UTF8.GetString(bytes);
+
+                Console.WriteLine($"Messaggio ricevuto: {text}");
+
+                await client.CompleteAsync(message);
+            }
+        }
+    }
+}
---
+++ MasterOfTornelli/Program.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Azure.Devices;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterOfTornelli
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            var configuration = builder.Build();
+
+            var serviceClient = 
+                ServiceClient.CreateFromConnectionString(
+                    configuration["IoTHubConnectionString"]);
+
+            Console.WriteLine("Hello Master of the Tornellis!");
+            Console.WriteLine("I'm Cortana!");
+            while (true)
+            {
+                Console.Write("What do you want to do now? ");
+                var commandText = Console.ReadLine();
+                var commandParts = commandText.ToLower().Split();
+
+                switch (commandParts[0])
+                {
+                    case "opendoor":
+                        await OpenDoor(serviceClient, commandParts);
+                        break;
+                    case "closedoor":
+                        await CloseDoor(serviceClient, commandParts);
+                        break;
+                    default:
+                        Console.WriteLine("Scrivi meglio, la prossima volta!");
+                        break;
+                }
+            }
+        }
+
+        private static async Task OpenDoor(ServiceClient serviceClient, string[] commandParts)
+        {
+            var bytes = Encoding.UTF8.GetBytes("opendoor");
+            var message = new Message(bytes);
+            try
+            {
+                await serviceClient.SendAsync(commandParts[1], message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ehila furbastro! Serve documentazione?");
+                Console.WriteLine("Scrivi: opendoor <idtornello>");
+            }
+        }
+
+        private static async Task CloseDoor(ServiceClient serviceClient, string[] commandParts)
+        {
+            var bytes = Encoding.UTF8.GetBytes("closedoor");
+            var message = new Message(bytes);
+            try
+            {
+                await serviceClient.SendAsync(commandParts[1], message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ehila furbastro! Serve documentazione?");
+                Console.WriteLine("Scrivi: closedoor <idtornello>");
+            }
+        }
+    }
+}
